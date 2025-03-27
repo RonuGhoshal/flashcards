89e29e1891540646224380f677e9496624653da2
Ronesh Ghoshal <roneshghoshal@roneshs-mbp.lan>
2025-03-27 13:57:54 -0400

    Add speech synthesis.

--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -9,10 +9,21 @@ const CardContent = ({ imageUrl, word, cardSide }) => {
         if (index < word.length) {
           const beforeLetter = word.substring(0, index);
           const highlightedLetter = word.substring(index, index + 1);
+          speechSynthesis.speak(
+            new SpeechSynthesisUtterance(highlightedLetter.toLowerCase())
+          );
           const afterLetter = word.substring(index + 1);
           document.querySelector(
             ".card-word"
           ).innerHTML = `${beforeLetter}<span style="text-decoration: underline;">${highlightedLetter}</span>${afterLetter}`;
+          index++;
+        } else if (index === word.length) {
+          document.querySelector(
+            ".card-word"
+          ).innerHTML = `<span style="text-decoration: underline;">${word}</span>`;
+          speechSynthesis.speak(
+            new SpeechSynthesisUtterance(word.toLowerCase())
+          );
           index++;
         } else {
           clearInterval(interval);
