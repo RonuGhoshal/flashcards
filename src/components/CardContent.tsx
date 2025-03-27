91bb793089e487ffc800ebcd3ff980669acc5551
Ronesh Ghoshal <roneshghoshal@roneshs-mbp.lan>
2025-03-27 14:26:57 -0400

    Refactor for readability.

--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -9,41 +9,57 @@ interface CardContentProps {
 
 const CardContent = ({ imageUrl, word, cardSide }: CardContentProps) => {
   const [renderedWord, setRenderedWord] = useState(word);
-  
-  useEffect(() => {
-    if (cardSide === "back") {
+
+  const highlightAndSpeakWord = () => {
+    setRenderedWord(`<span style="text-decoration: underline;">${word}</span>`);
+    speechSynthesis.speak(new SpeechSynthesisUtterance(word.toLowerCase()));
+  };
+
+  const highlightAndSpeakLetter = (index: number, word: string) => {
+    const beforeLetter = word.substring(0, index);
+    const highlightedLetter = word.substring(index, index + 1);
+    speechSynthesis.speak(
+      new SpeechSynthesisUtterance(highlightedLetter.toLowerCase())
+    );
+    const afterLetter = word.substring(index + 1);
+    setRenderedWord(
+      `${beforeLetter}<span style="text-decoration: underline;">${highlightedLetter}</span>${afterLetter}`
+    );
+  };
+
+  const highlightAndSpeak = () => {
+    setTimeout(() => {
+      highlightAndSpeakWord();
       let index = 0;
       const interval = setInterval(() => {
         if (index < word.length) {
-          const beforeLetter = word.substring(0, index);
-          const highlightedLetter = word.substring(index, index + 1);
-          speechSynthesis.speak(
-            new SpeechSynthesisUtterance(highlightedLetter.toLowerCase())
-          );
-          const afterLetter = word.substring(index + 1);
-          setRenderedWord(
-            `${beforeLetter}<span style="text-decoration: underline;">${highlightedLetter}</span>${afterLetter}`
-          );
+          highlightAndSpeakLetter(index, word);
           index++;
         } else if (index === word.length) {
-          setRenderedWord(`<span style="text-decoration: underline;">${word}</span>`);
-          speechSynthesis.speak(
-            new SpeechSynthesisUtterance(word.toLowerCase())
-          );
+          highlightAndSpeakWord();
           index++;
         } else {
           clearInterval(interval);
         }
       }, 1000);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (cardSide === "back") {
+      highlightAndSpeak();
     }
-  }, [cardSide, word]);
+  }, [cardSide]);
 
   return (
     <div className="card-content">
       <div className="card-image">
         <img src={imageUrl} alt={word} />
       </div>
-      <div className="card-word" dangerouslySetInnerHTML={{ __html: renderedWord }} />
+      <div
+        className="card-word"
+        dangerouslySetInnerHTML={{ __html: renderedWord }}
+      />
     </div>
   );
 };
