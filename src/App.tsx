2577a7ab42d899098539f9d620b67bf8d476df9b
Ronesh Ghoshal <roneshghoshal@roneshs-mbp.lan>
2025-03-27 17:23:14 -0400

    Add some more animal cards.

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,22 @@ const cardInfo = [
     imageUrl:
       "https://thumbs.dreamstime.com/b/gold-fish-white-background-25937234.jpg",
   },
+  {
+    word: "BIRD",
+    imageUrl:
+      "https://media.istockphoto.com/id/626132614/photo/blue-fronted-redstart-the-beautiful-blue.jpg?s=612x612&w=0&k=20&c=V9ZpAJrnOPxgf9TiT3wxk77V2TbKqiXl_5yOBL1DP_c=",
+  },
+  { word: "BAT",
+    imageUrl: "https://www.shutterstock.com/image-photo/bat-flying-isolated-on-white-600nw-2490998921.jpg",
+  },
+  {
+    word: "MOUSE",
+    imageUrl: "https://t3.ftcdn.net/jpg/07/66/01/40/360_F_766014094_RV83tf5oy9t8ejXfescEot8qDVjjHazl.jpg",
+  },
+  {
+    word: "LION",
+    imageUrl: "https://media.istockphoto.com/id/455700809/photo/lion-standing-panthera-leo-10-years-old-isolated-on-white.jpg?s=612x612&w=0&k=20&c=wQgwZwid0UZ08mLL8sS1c2TPYZFzaTz9Xe4SDtSkmQk=",
+  }
 ];
 function App() {
   const [index, setIndex] = useState(0);
