2c97eeeb6adbf1fad7353f12e86d550b856da244
Ronesh Ghoshal <roneshghoshal@roneshs-mbp.lan>
2025-03-27 13:49:38 -0400

    Highlight letters on reverse side of card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,27 @@
-import { useState } from 'react';
-import './Card.css';
-import CardContent from './CardContent';
+import { useState } from "react";
+import "./Card.css";
+import CardContent from "./CardContent";
 
-const Card = ({word = "CAT", imageUrl = "https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=1024x1024&w=is&k=20&c=QaEkKC7lFEBrzzPftMRBVuOZq4FNOnUjOV1VqTmpMfY="}) => {
-    const [isFlipped, setIsFlipped] = useState(false);
+const Card = ({
+  word = "CATATONIC",
+  imageUrl = "https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=1024x1024&w=is&k=20&c=QaEkKC7lFEBrzzPftMRBVuOZq4FNOnUjOV1VqTmpMfY=",
+}) => {
+  const [isFlipped, setIsFlipped] = useState(false);
 
-    const handleClick = () => {
-        setIsFlipped(!isFlipped);
-    };
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
 
-    return (
-        <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
-            <div className="card-front"><CardContent word={word} imageUrl={imageUrl} /></div>
-            <div className="card-back"><CardContent word={word} imageUrl={imageUrl} /></div>
-        </div>
-    );
+  return (
+    <div className={`card ${isFlipped ? "flipped" : ""}`} onClick={handleClick}>
+      <div className="card-front">
+        {!isFlipped && <CardContent cardSide="front" word={word} imageUrl={imageUrl} />}
+      </div>
+      <div className="card-back">
+        {isFlipped && <CardContent cardSide="back" word={word} imageUrl={imageUrl} />}
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -1,14 +1,34 @@
-import './CardContent.css';
+import { useEffect } from "react";
+import "./CardContent.css";
 
-const CardContent = ({imageUrl, word}) => {
-    return (
-        <div className="card-content">
-            <div className="card-image">
-                <img src={imageUrl} alt={word} />
-            </div>
-            <div className="card-word">{word}</div>
-        </div>
-    );
+const CardContent = ({ imageUrl, word, cardSide }) => {
+  useEffect(() => {
+    if (cardSide === "back") {
+      let index = 0;
+      const interval = setInterval(() => {
+        if (index < word.length) {
+          const beforeLetter = word.substring(0, index);
+          const highlightedLetter = word.substring(index, index + 1);
+          const afterLetter = word.substring(index + 1);
+          document.querySelector(
+            ".card-word"
+          ).innerHTML = `${beforeLetter}<span style="text-decoration: underline;">${highlightedLetter}</span>${afterLetter}`;
+          index++;
+        } else {
+          clearInterval(interval);
+        }
+      }, 1000);
+    }
+  }, [cardSide]);
+
+  return (
+    <div className="card-content">
+      <div className="card-image">
+        <img src={imageUrl} alt={word} />
+      </div>
+      <div className="card-word">{word}</div>
+    </div>
+  );
 };
 
 export default CardContent;
